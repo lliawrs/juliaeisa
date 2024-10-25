095890c33bcb33ed2684e53ae224033804bd04c8
lia <140445895+lliawrs@users.noreply.github.com>
2024-10-25 11:21:53 -0300

    ee

--- main.js
+++ main.js
@@ -18,7 +18,10 @@ let respostaEstaVisivel = false;
 
 function viraCartao(){
     respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active',respostaEstaVisivel);
 }
+
+cartao.addEventListener('click', viraCartao)
 
 container.appendChild(cartao);
 }
