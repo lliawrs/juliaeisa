fc7b54a5d77c17c1b12efa55ba263e1ab205c192
lia <140445895+lliawrs@users.noreply.github.com>
2024-10-25 12:23:09 -0300

    ..

--- pag2.js
+++ pag2.js
@@ -3,7 +3,7 @@ function criaCartão(categoria, pergunta, resposta){
     let cartao = document.createElement("article");
     cartao.className = "cartao";
     cartao.innerHTML = `
-    <div class="conteudo-cartao">
+    <div class="conteudo-cartao" onclick="mostrarResposta(this)">
         <h3>${categoria}</h3>
         <div class="pergunta-cartao">
             <p>${pergunta}</p>
@@ -16,7 +16,12 @@ function criaCartão(categoria, pergunta, resposta){
     container.appendChild(cartao);
     }
 
-    function mostrarResposta(){
-        const resposta = document.querySelector('.resposta-cartao');
-        
+    function mostrarResposta(card){
+        const resposta = card.querySelector('.resposta-cartao');
+        if(resposta.style.display === 'none'){
+           resposta.style.display = 'block'
+        }
+        else{
+           resposta.style.display = 'none'
+        }
     }
